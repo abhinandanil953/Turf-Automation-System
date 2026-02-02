ce772c775dadb11d01a3649f536b5e62d9392262
Abhinand Anil <abhinandanil953@gmail.com>
2026-02-02 18:34:45 +0530

    Add files via upload

---
+++ run_add_daily_slots.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\xampp\php\php.exe C:\xampp\htdocs\turfbooking\admin\add_daily_slots.php
---
+++ run_delete_expired_slots.bat
@@ -0,0 +1,2 @@
+@echo off
+C:\xampp\php\php.exe C:\xampp\htdocs\turfbooking\admin\delete_expired_slots.php
